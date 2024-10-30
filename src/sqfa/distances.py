6d8431ac064379aa671127b20f553b6d8d0c206b
dherresp-adm <dherrera1911@gmail.com>
2024-10-30 12:26:19 -0400

    Add non-squared distance

--- src/sqfa/distances.py
+++ src/sqfa/distances.py
@@ -34,3 +34,22 @@ def ai_distance_sq(A, B):
     # Compute the distance
     distance_squared = torch.sum(torch.log(gen_eigvals) ** 2, axis=-1)
     return distance_squared
+
+
+def ai_distance(A, B):
+    """
+    Compute the affine invariant distance between SPD matrices.
+
+    Parameters
+    ----------
+    A : torch.Tensor
+        Shape (n_batch_A, n_dim, n_dim), the first SPD matrix.
+    B : torch.Tensor
+        Shape (n_batch_B, n_dim, n_dim), the second SPD matrix.
+
+    Returns
+    -------
+    distance : torch.Tensor
+        Shape (n_batch_A, n_batch_B), the affine invariant distance.
+    """
+    return torch.sqrt(ai_distance_sq(A, B))
