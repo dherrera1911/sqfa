988addcfd146756f75d0f5a20f267289ba4a6f77
dherresp-adm <dherrera1911@gmail.com>
2024-10-30 11:37:20 -0400

    Add optim and docstring

--- src/sqfa/__init__.py
+++ src/sqfa/__init__.py
@@ -1,5 +1,6 @@
+"""Supervised Quadratic Feature Analysis (SQFA) package."""
+from . import _optim as _optim
 from . import constrains as constrains
 from . import distances as distances
 from . import linalg_utils as linalg_utils
 from . import model as model
-from . import _optim as _optim
