5500a207ad233b0ee0169a1f899ef2815aa0eedd
dherresp-adm <dherrera1911@gmail.com>
2024-10-31 23:52:52 -0400

    Change constraints module name

--- src/sqfa/__init__.py
+++ src/sqfa/__init__.py
@@ -1,6 +1,6 @@
 """Supervised Quadratic Feature Analysis (SQFA) package."""
 from . import _optim as _optim
-from . import constrains as constrains
+from . import constraints as constraints
 from . import distances as distances
 from . import linalg_utils as linalg_utils
 from . import model as model
