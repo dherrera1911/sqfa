690dd4dc55f0329d92a1a436aafceec0b0e8d956
dherresp-adm <dherrera1911@gmail.com>
2024-11-12 13:54:35 -0500

    Add log euclidean

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -1,4 +1,5 @@
 """Tests for the distances module."""
+
 import geomstats.geometry.spd_matrices as spd_matrices
 import pytest
 import torch
@@ -6,10 +7,11 @@ import torch
 from make_examples import sample_spd
 from sqfa.distances import (
     affine_invariant_sq,
+    log_euclidean_sq,
 )
 
 
-def affine_invariant_ref(A, B):
+def distance_ref(A, B, metric):
     """
     Compute the affine invariant distance between pairs (A, B) using geomstats.
 
@@ -28,10 +30,17 @@ def affine_invariant_ref(A, B):
     n_dim = A.shape[1]
 
     # Initialize manifold
-    manifold = spd_matrices.SPDMatrices(n=n_dim, equip=spd_matrices.SPDAffineMetric)
+    manifold = spd_matrices.SPDMatrices(
+        n=n_dim,
+        equip=False,
+    )
+    if metric == "affine":
+        manifold.equip_with_metric(spd_matrices.SPDAffineMetric)
+    elif metric == "log_euclidean":
+        manifold.equip_with_metric(spd_matrices.SPDLogEuclideanMetric)
 
     distances_geomstats = manifold.metric.squared_dist(
-      point_a = A.numpy()[:,None,:,:], point_b = B.numpy()[None,:,:,:]
+        point_a=A.numpy()[:, None, :, :], point_b=B.numpy()[None, :, :, :]
     )
 
     return torch.squeeze(torch.as_tensor(distances_geomstats, dtype=torch.float32))
@@ -48,12 +57,20 @@ def sample_spd_matrices(n_matrices_A, n_matrices_B, n_dim):
 @pytest.mark.parametrize("n_matrices_A", [1, 4, 8])
 @pytest.mark.parametrize("n_matrices_B", [1, 4, 8])
 @pytest.mark.parametrize("n_dim", [2, 4, 6])
-def test_generalized_eigenvalues(sample_spd_matrices, n_matrices_A,
-                                 n_matrices_B, n_dim):
+def test_distance_sq(sample_spd_matrices, n_matrices_A, n_matrices_B, n_dim):
     """Test the generalized eigenvalues function."""
     A, B = sample_spd_matrices
 
-    distances = affine_invariant_sq(A, B)
-    distances_geomstats = affine_invariant_ref(A, B)
+    ai_distances = affine_invariant_sq(A, B)
+    ai_distances_ref = distance_ref(A, B, "affine")
 
-    assert torch.allclose(distances, distances_geomstats, atol=1e-5)
+    assert torch.allclose(
+        ai_distances, ai_distances_ref, atol=1e-5
+    ), "The affine invariant distance is not correct."
+
+    le_distances = log_euclidean_sq(A, B)
+    le_distances_ref = distance_ref(A, B, "log_euclidean")
+
+    assert torch.allclose(
+        le_distances, le_distances_ref, atol=1e-5
+    ), "The log euclidean distance is not correct."
