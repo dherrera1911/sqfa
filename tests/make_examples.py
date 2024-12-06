e79a18f8fc8f0a00f0ed5636adf6b65100acc342
dherresp-adm <dherrera1911@gmail.com>
2024-12-06 15:09:11 -0500

    Remove geomstats dependency

--- tests/make_examples.py
+++ tests/make_examples.py
@@ -1,11 +1,18 @@
 """Generate synthetic data for testing."""
 
-import geomstats.geometry.spd_matrices as spd
 import torch
 
+def make_orthogonal_matrices(n_matrices, n_dim):
+    """Generate random orthogonal matrices."""
+    low_tri = torch.randn(n_matrices, n_dim, n_dim)
+    low_tri = torch.tril(low_tri, diagonal=-1)
+    skew_sym = low_tri - low_tri.transpose(1, 2)
+    orthogonal = torch.matrix_exp(skew_sym)
+    return orthogonal
 
 def sample_spd(n_matrices, n_dim):
     """Generate random SPD matrices."""
-    manifold = spd.SPDMatrices(n=n_dim, equip=spd.SPDAffineMetric)
-    covariances = manifold.random_point(n_samples=n_matrices)
-    return torch.as_tensor(covariances, dtype=torch.float32)
+    eigvals = 2 * (torch.rand(n_matrices, n_dim)) ** 2 + 0.01
+    eigvecs = make_orthogonal_matrices(n_matrices, n_dim)
+    spd = torch.einsum('ijk,ik,ikl->ijl', eigvecs, eigvals, eigvecs.transpose(1, 2))
+    return torch.squeeze(spd)
