b4bd39dfaa7eba6e14b5142d2e397e4f4428bca9
dherresp-adm <dherrera1911@gmail.com>
2025-01-11 12:45:01 -0300

    More descriptive matrix names

--- tests/test_distances.py
+++ tests/test_distances.py
@@ -7,36 +7,43 @@ from make_examples import sample_spd
 from sqfa.distances import (
     affine_invariant_sq,
     log_euclidean_sq,
+    fisher_rao_lower_bound,
 )
 
 torch.set_default_dtype(torch.float64)
 
 
 @pytest.fixture(scope="function")
-def sample_spd_matrices(n_matrices_A, n_dim):
+def sample_spd_matrices(n_classes, n_dim):
     """Generate a tensor of SPD matrices."""
-    A = sample_spd(n_matrices_A, n_dim)
+    spd_mat = sample_spd(n_classes, n_dim)
+    return spd_mat
+
+@pytest.fixture(scope="function")
+def sample_vectors(n_classes, n_dim):
+    """Generate a tensor of vectors."""
+    A = torch.randn(n_classes, n_dim)
     return A
 
 
 def get_diag(A):
-    """Get the diagonal of a tensor."""
+    """Get the diagonal of a tensor, even when tensor is a scalar."""
     if A.dim() > 0:
         return A.diagonal(dim1=-2, dim2=-1)
     else:
         return A
 
 
-@pytest.mark.parametrize("n_matrices_A", [1, 4, 8])
+@pytest.mark.parametrize("n_classes", [1, 4, 8])
 @pytest.mark.parametrize("n_dim", [2, 4, 6])
-def test_distance_sq(sample_spd_matrices, n_matrices_A, n_dim):
+def test_distance_sq(sample_spd_matrices, n_classes, n_dim):
     """Test the generalized eigenvalues function."""
-    A = sample_spd_matrices
+    spd_mat = sample_spd_matrices
 
-    ai_distances_sq = affine_invariant_sq(A, A)
+    ai_distances_sq = affine_invariant_sq(spd_mat, spd_mat)
 
-    if n_matrices_A != 1:
-        assert ai_distances_sq.shape == (n_matrices_A, n_matrices_A)
+    if n_classes != 1:
+        assert ai_distances_sq.shape == (n_classes, n_classes)
     else:
         assert ai_distances_sq.shape == ()
 
@@ -45,21 +52,21 @@ def test_distance_sq(sample_spd_matrices, n_matrices_A, n_dim):
     ), "The self-distance matrix for AIRM is not symmetric"
 
     assert torch.allclose(
-        get_diag(ai_distances_sq), torch.zeros(n_matrices_A), atol=1e-5
+        get_diag(ai_distances_sq), torch.zeros(n_classes), atol=1e-5
     ), "The diagonal of the self-distance matrix for AIRM is not zero"
 
-    A_inv = torch.inverse(A)
+    spd_inv = torch.inverse(spd_mat)
 
-    ai_distances_inv_sq = affine_invariant_sq(A_inv, A_inv)
+    ai_distances_inv_sq = affine_invariant_sq(spd_inv, spd_inv)
 
     assert torch.allclose(
         ai_distances_sq, ai_distances_inv_sq, atol=1e-5
     ), "The affine invariant distance is not invariant to inversion."
 
-    le_distances_sq = log_euclidean_sq(A, A)
+    le_distances_sq = log_euclidean_sq(spd_mat, spd_mat)
 
-    if n_matrices_A != 1:
-        assert le_distances_sq.shape == (n_matrices_A, n_matrices_A)
+    if n_classes != 1:
+        assert le_distances_sq.shape == (n_classes, n_classes)
     else:
         assert le_distances_sq.shape == ()
 
@@ -68,20 +75,21 @@ def test_distance_sq(sample_spd_matrices, n_matrices_A, n_dim):
     ), "The self-distance matrix for AIRM is not symmetric"
 
     assert torch.allclose(
-        get_diag(le_distances_sq), torch.zeros(n_matrices_A), atol=1e-5
+        get_diag(le_distances_sq), torch.zeros(n_classes), atol=1e-5
     ), "The diagonal of the self-distance matrix for AIRM is not zero"
 
-    le_distances_inv_sq = log_euclidean_sq(A_inv, A_inv)
+    le_distances_inv_sq = log_euclidean_sq(spd_inv, spd_inv)
 
     assert torch.allclose(
         le_distances_sq, le_distances_inv_sq, atol=1e-5
     ), "The log-Euclidean distance is not invariant to inversion."
 
-    B = torch.eye(n_dim)
+    eye = torch.eye(n_dim)
 
-    ai_dist_to_eye = affine_invariant_sq(A, B)
-    le_dist_to_eye = log_euclidean_sq(A, B)
+    ai_dist_to_eye = affine_invariant_sq(spd_mat, eye)
+    le_dist_to_eye = log_euclidean_sq(spd_mat, eye)
 
     assert torch.allclose(
         ai_dist_to_eye, le_dist_to_eye, atol=1e-5
     ), "The AIRM and LE distances from the identity are not equal."
+
